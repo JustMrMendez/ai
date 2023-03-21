800f949c379f03e72231766902fb6b8eb92c3c8d
JustMrMendez <mrmendez@amvestor.com>
2023-03-21 15:51:27 -0400

    t1

--- lib/tree.py
+++ lib/tree.py
@@ -11,7 +11,7 @@ def get_file_metadata(file_path):
     return metadata
 
 
-def generate_tree(folder_path, relative_path=""):
+def generate_tree2(folder_path, relative_path=""):
     tree = {}
     if os.path.isdir(folder_path):
         tree['type'] = 'folder'
@@ -39,4 +39,57 @@ def generate_tree(folder_path, relative_path=""):
     else:
         print("Error: The provided path is not a folder!")
 
+    return tree
+
+
+def generate_tree(folder_path, relative_path="", duplicates=None):
+    if duplicates is None:
+        duplicates = {}
+
+    tree = {}
+    if os.path.isdir(folder_path):
+        tree['type'] = 'folder'
+        tree['name'] = os.path.basename(folder_path)
+        tree['relative_path'] = relative_path
+        tree['folder_path'] = folder_path
+        tree['files'] = []
+        tree['subfolders'] = []
+
+        for item in os.listdir(folder_path):
+            item_path = os.path.join(folder_path, item)
+            item_relative_path = os.path.join(relative_path, item)
+
+            if os.path.isdir(item_path):
+                subfolder_tree = generate_tree(
+                    item_path, item_relative_path, duplicates)
+                tree['subfolders'].append(subfolder_tree)
+            else:
+                file_info = {
+                    'name': item,
+                    'relative_path': item_relative_path,
+                    'metadata': get_file_metadata(item_path),
+                }
+
+                if file_info['name'] in duplicates:
+                    duplicates[file_info['name']].append(item_relative_path)
+                    current_file_mtime = os.path.getmtime(item_path)
+                    parent_file_mtime = os.path.getmtime(
+                        duplicates[file_info['name']][-1])
+
+                    if current_file_mtime > parent_file_mtime:
+                        duplicates[file_info['name']][-1], duplicates[file_info['name']
+                                                                      ][-2] = duplicates[file_info['name']][-2], duplicates[file_info['name']][-1]
+
+                else:
+                    duplicates[file_info['name']] = [item_relative_path]
+
+                if item_relative_path != duplicates[file_info['name']][-1]:
+                    continue
+
+                tree['files'].append(file_info)
+
+    else:
+        print("Error: The provided path is not a folder!")
+
+    tree['duplicates'] = duplicates
     return tree
